46afa68cfb55bc7d97f8828f91d7c14ea141b68d
Junsuk Oh <a811219@gmail.com>
2019-03-21 09:58:59 +0900

    컬러 설정 후 화면 회전 처리 추가

--- app/src/main/java/com/example/recyclerviewexam/exam283/ColorFragment.java
+++ app/src/main/java/com/example/recyclerviewexam/exam283/ColorFragment.java
@@ -1,6 +1,8 @@
 package com.example.recyclerviewexam.exam283;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,18 +18,39 @@ import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
 public class ColorFragment extends Fragment {
+    private int mColor;
+
     public ColorFragment() {
         // Required empty public constructor
     }
 
-    public static ColorFragment newInstance() {
-        return new ColorFragment();
+    public static ColorFragment newInstance(int color) {
+        ColorFragment fragment = new ColorFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt("color", color);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            mColor = getArguments().getInt("color");
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_color, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setColor(mColor);
     }
 
     public void setColor(int color) {
