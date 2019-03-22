f3616b37c6a0ed8231938f054baea61d636552b0
Junsuk Oh <a811219@gmail.com>
2019-03-22 11:35:56 +0900

    ConstraintSet 애니메이션 연습 예제

--- app/src/main/java/com/example/recyclerviewexam/resexam/ResExamActivity.java
+++ app/src/main/java/com/example/recyclerviewexam/resexam/ResExamActivity.java
@@ -3,8 +3,11 @@ package com.example.recyclerviewexam.resexam;
 import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.transition.TransitionManager;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -12,12 +15,19 @@ import android.view.animation.AnimationUtils;
 import com.example.recyclerviewexam.R;
 
 public class ResExamActivity extends AppCompatActivity {
+    ConstraintSet mConstraintSet1 = new ConstraintSet();
+    ConstraintSet mConstraintSet2 = new ConstraintSet();
+    ConstraintLayout mConstraintLayout;
+    boolean mOld = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_res_exam);
 
+        mConstraintSet2.clone(this, R.layout.activity_res_exam2);
+        setContentView(R.layout.activity_res_exam);
+        mConstraintLayout = findViewById(R.id.activity_main);
+        mConstraintSet1.clone(mConstraintLayout);
     }
 
     public void onClick(View view) {
@@ -32,5 +42,14 @@ public class ResExamActivity extends AppCompatActivity {
         ActivityOptions options = ActivityOptions
                 .makeSceneTransitionAnimation(this, view, "icon");
         ActivityCompat.startActivity(this, intent, options.toBundle());
+    }
+
+    public void onButtonClicked(View view) {
+        TransitionManager.beginDelayedTransition(mConstraintLayout);
+        if (mOld = !mOld) {
+            mConstraintSet1.applyTo(mConstraintLayout);
+        }  else {
+            mConstraintSet2.applyTo(mConstraintLayout);
+        }
     }
 }
