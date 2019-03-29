09b32ada34ac1139b9ade0b8298303dd8ab2dc17
Junsuk Oh <a811219@gmail.com>
2019-03-29 09:33:34 +0900

    DataBinding - Observable objects

--- app/src/main/java/com/example/recyclerviewexam/databinding/DataBindingActivity.java
+++ app/src/main/java/com/example/recyclerviewexam/databinding/DataBindingActivity.java
@@ -3,21 +3,26 @@ package com.example.recyclerviewexam.databinding;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.example.recyclerviewexam.R;
 
 public class DataBindingActivity extends AppCompatActivity {
 
+    User user = new User("준석", "오");
+    private ActivityDataBindingBinding mBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityDataBindingBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_data_binding);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_data_binding);
 
 
-        User user = new User("준석", "오");
-        binding.setUser(user);
+        mBinding.setUser(user);
+        mBinding.myName.setText("영석");
+    }
 
-
-        binding.firstName.setText("영석");
+    public void onValueChange(View view) {
+        user.setFirstName("마이클");
     }
 }
--- app/src/main/java/com/example/recyclerviewexam/databinding/User.java
+++ app/src/main/java/com/example/recyclerviewexam/databinding/User.java
@@ -1,6 +1,11 @@
 package com.example.recyclerviewexam.databinding;
 
-public class User {
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+import com.android.databinding.library.baseAdapters.BR;
+
+public class User extends BaseObservable {
     private String firstName;
     private String lastName;
 
@@ -9,19 +14,23 @@ public class User {
         this.lastName = lastName;
     }
 
+    @Bindable
     public String getFirstName() {
         return firstName;
     }
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
+        notifyPropertyChanged(BR.firstName);
     }
 
+    @Bindable
     public String getLastName() {
         return lastName;
     }
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+        notifyPropertyChanged(BR.lastName);
     }
 }
