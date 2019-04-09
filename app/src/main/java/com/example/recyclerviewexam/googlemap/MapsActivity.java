de636ea4d34b8546b1c0dc160b3eabc9da0d0a59
Junsuk Oh <a811219@gmail.com>
2019-04-09 13:50:27 +0900

    TedPermission 으로 런타임 권한체크 쉽게 하기

--- app/src/main/java/com/example/recyclerviewexam/googlemap/MapsActivity.java
+++ app/src/main/java/com/example/recyclerviewexam/googlemap/MapsActivity.java
@@ -2,18 +2,12 @@ package com.example.recyclerviewexam.googlemap;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AlertDialog;
 import android.widget.Toast;
 
 import com.example.recyclerviewexam.R;
@@ -29,6 +23,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
 
 import java.io.IOException;
 import java.util.List;
@@ -116,38 +112,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         });
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
-            // Permission is not granted
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.ACCESS_FINE_LOCATION)) {
-                // 사용자가 권한 거부를 한번 이상 했던적이 있으면
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
-                                Manifest.permission.ACCESS_COARSE_LOCATION},
-                        MY_PERMISSIONS_REQUEST_FINE_LOCATION);
-
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-            } else {
-                // 퍼미션 요청
-                // No explanation needed; request the permission
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
-                                Manifest.permission.ACCESS_COARSE_LOCATION},
-                        MY_PERMISSIONS_REQUEST_FINE_LOCATION);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
-
-        } else {
-            // 권한이 있다면
-            mMap.setMyLocationEnabled(true);
-        }
+        TedPermission.with(this)
+                .setPermissionListener(permissionlistener)
+                .setDeniedMessage("설정에서 언제든지 권한을 변경할 수 있습니다.")
+                .setPermissions(Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION)
+                .check();
 
 
         // Add a marker in Sydney and move the camera
@@ -156,42 +126,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13.0f));
     }
 
-    @SuppressLint("MissingPermission")
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String[] permissions, int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_FINE_LOCATION: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // 허락 됨
-                    mMap.setMyLocationEnabled(true);
-                    startLocationUpdates();
-                } else {
-                    // 거부 됨
-                    // 다이얼로그 띄우기
-                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                    builder.setTitle("권한")
-                            .setMessage("설정에서 언제든지 권한을 변경할 수 있습니다.")
-                            .setPositiveButton("설정", (dialog, id) -> {
-                                // 설정화면
-                                Intent intent = new Intent();
-                                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-                                Uri uri = Uri.fromParts("package", getPackageName(), null);
-                                intent.setData(uri);
-                                startActivity(intent);
-                            })
-                            .setNegativeButton("닫기", null)
-                            .show();
-                }
-                return;
-            }
-
-            // other 'case' lines to check for other
-            // permissions this app might request.
-        }
-    }
 
     @Override
     protected void onResume() {
@@ -200,20 +134,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     private void startLocationUpdates() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // 거부됨
-        } else {
-            // 허락됨
-            LocationRequest request = new LocationRequest();
-            request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-            request.setInterval(10000);
-            request.setFastestInterval(5000);
-
-            mFusedLocationProviderClient.requestLocationUpdates(
-                    request,
-                    mLocationCallback,
-                    null /* Looper */);
-        }
+        TedPermission.with(this)
+                .setPermissionListener(permissionlistener2)
+                .setDeniedMessage("설정에서 언제든지 권한을 변경할 수 있습니다.")
+                .setPermissions(Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION)
+                .check();
     }
 
     @Override
@@ -226,4 +152,45 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mFusedLocationProviderClient.removeLocationUpdates(mLocationCallback);
     }
 
+
+    @SuppressLint("MissingPermission")
+    PermissionListener permissionlistener = new PermissionListener() {
+        @Override
+        public void onPermissionGranted() {
+            // 허락 됨
+            mMap.setMyLocationEnabled(true);
+            startLocationUpdates();
+        }
+
+        @Override
+        public void onPermissionDenied(List<String> deniedPermissions) {
+
+        }
+
+
+    };
+
+    @SuppressLint("MissingPermission")
+    PermissionListener permissionlistener2 = new PermissionListener() {
+        @Override
+        public void onPermissionGranted() {
+            // 허락됨
+            LocationRequest request = new LocationRequest();
+            request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            request.setInterval(10000);
+            request.setFastestInterval(5000);
+
+            mFusedLocationProviderClient.requestLocationUpdates(
+                    request,
+                    mLocationCallback,
+                    null /* Looper */);
+        }
+
+        @Override
+        public void onPermissionDenied(List<String> deniedPermissions) {
+
+        }
+
+
+    };
 }
